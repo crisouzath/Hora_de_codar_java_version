f2f83c6e59b8101a2755b391248f5bb7489ed94e
Cristhian de Souza <111239606+crisouzath@users.noreply.github.com>
2022-11-22 23:35:38 -0300

    Create 1.baby_steps.java

---
+++ 1.baby_steps.java
@@ -0,0 +1,10 @@
+/* Escreva um programa em Java e crie uma variável chamada "Planeta" e atribua-a um valor "Plutão". 
+
+Exiba o valor para o usuário. */
+
+class Main {
+  public static void main(String[] args) {
+    String planeta="Plutão";
+    System.out.println(planeta);
+  }
+}
